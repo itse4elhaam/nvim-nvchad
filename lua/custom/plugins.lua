ed83b048a44374f24cc80fbad9f50bf0c9eac140
Elhaam <e4elhaam@gmail.com>
2024-07-27 20:30:04 +0500

    feat: auto save, template string

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,25 +1,61 @@
 local plugins = {
-{ 'wakatime/vim-wakatime', lazy = false },
   {
-    'kevinhwang91/nvim-ufo',
-    requires = 'kevinhwang91/promise-async',
-    config = require "custom.configs.ufo"
-  },
-  {
-    '0x00-ketsu/autosave.nvim',
-    event = { "InsertLeave", "TextChanged" },
+    "axelvc/template-string.nvim",
     config = function()
-      require('autosave').setup {}
+      require("axelvc/template-string.nvim").setup({
+        filetypes = { "html", "typescript", "javascript", "typescriptreact", "javascriptreact", "vue", "svelte", "python" },
+        jsx_brackets = true,
+        remove_template_string = false,
+        restore_quotes = {
+          normal = [["]],
+          jsx = [["]],
+        },
+      })
     end
   },
+  { "wakatime/vim-wakatime", lazy = false },
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "gopls",
-      },
+    "kevinhwang91/nvim-ufo",
+    requires = "kevinhwang91/promise-async",
+    config = require "custom.configs.ufo"
+  },
+  -- todo move this to a seperate file
+  {
+    "0x00-ketsu/autosave.nvim",
+    event = { "InsertLeave", "TextChanged" },
+    config = function()
+      require("autosave").setup {
+        -- You can put any autosave specific configurations here
+        enabled = true, -- Enable auto-save
+        execution_message = "Auto-saved at: " .. vim.fn.strftime("%H:%M:%S"),
+        events = { "InsertLeave", "TextChanged" },
+        conditions = {
+          exists = true,
+          filename_is_not = {},
+          filetype_is_not = {},
+          modifiable = true
+        },
+        write_all_buffers = false,
+        on_off_commands = true,
+        clean_command_line_interval = 0,
+        debounce_delay = 135
+      }
+      local autosave_group = vim.api.nvim_create_augroup("AutoSaveFormatting", { clear = true })
+      vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+        group = autosave_group,
+        callback = function()
+          vim.lsp.buf.format { async = false }
+        end,
+      })
+    end
+  }, {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "gopls",
     },
   },
+},
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -46,4 +82,5 @@ local plugins = {
     end,
   },
 }
+
 return plugins
