32ab3fd3b3963b1da423b859d06ae0be2350d849
Elhaam <e4elhaam@gmail.com>
2024-07-28 20:38:34 +0500

    fixes: code folding

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -3,3 +3,4 @@ local opt = vim.opt
 
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevelstart = 99
--- todo.md
+++ todo.md
@@ -1,5 +1,3 @@
-- fold in nvim
-
 # refactoring
 - manage plugins better
 - manage overriding vim settings better
