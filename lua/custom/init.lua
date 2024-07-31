6855c369899aa17c3f8836749581fc5179d7802f
Elhaam <e4elhaam@gmail.com>
2024-08-01 01:57:49 +0500

    feat: utils file

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,4 +1,3 @@
-local vim = vim
 local opt = vim.opt
 
 opt.foldmethod = "expr"
@@ -6,3 +5,12 @@ opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevelstart = 99
 
 opt.relativenumber = true
+
+-- look into this later
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   callback = function()
+--     -- Delay the execution to ensure all startup processes are complete
+--     local fn = require("custom.utils")
+--     vim.defer_fn(fn.open_nvim_tree_if_no_buffers, 100)
+--   end
+-- })
