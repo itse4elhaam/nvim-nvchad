371ea595e7070fdd0b9573ab499a64f150ab8062
Elhaam <e4elhaam@gmail.com>
2024-10-28 07:33:36 +0500

    feat: added tmux-window-name again

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -70,3 +70,13 @@ vim.api.nvim_create_autocmd("VimLeave", {
   -- this is for underscore
   command = "set guicursor=a:hor30",
 })
+
+local uv = vim.loop
+
+vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
+  callback = function()
+    if vim.env.TMUX_PLUGIN_MANAGER_PATH then
+      uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py", {})
+    end
+  end,
+})
