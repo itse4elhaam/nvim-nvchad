76c961ab114ecb38b2b434fe7d800dbf2c67518a
Elhaam <e4elhaam@gmail.com>
2024-11-13 01:08:34 +0500

    feat: lgit in neovim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,22 @@
 local plugins = {
   {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
