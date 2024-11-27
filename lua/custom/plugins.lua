285aaa8b4b6cee5cc5abbb952a763b6a118fb467
Elhaam <e4elhaam@gmail.com>
2024-11-27 12:40:37 +0500

    feat: quicker.nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,4 +1,14 @@
 local plugins = {
+  {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+    config = function ()
+          require("quicker").setup()
+    end
+  },
   -- TODO: organize this into files
   {
     "ThePrimeagen/harpoon",
