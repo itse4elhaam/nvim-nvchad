cb63af13829b53fa3a9be4ffd9da381a10226f52
Elhaam <e4elhaam@gmail.com>
2024-12-24 16:06:08 +0500

    refactor: blink.cmp

---
+++ lua/custom/configs/blink.cmp.lua
@@ -0,0 +1,81 @@
+local opts = {
+  completion = {
+    accept = {
+      -- experimental auto-brackets support
+      auto_brackets = {
+        enabled = true,
+      },
+    },
+    menu = {
+      scrollbar = false,
+      border = "rounded",
+      draw = {
+        columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+      },
+    },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 200,
+    },
+  },
+  keymap = {
+    preset = "super-tab",
+    ["<CR>"] = { "accept", "fallback" },
+    -- TODO: replace with a loop
+    ["<A-1>"] = {
+      function(cmp)
+        cmp.accept { index = 1 }
+      end,
+    },
+    ["<A-2>"] = {
+      function(cmp)
+        cmp.accept { index = 2 }
+      end,
+    },
+    ["<A-3>"] = {
+      function(cmp)
+        cmp.accept { index = 3 }
+      end,
+    },
+    ["<A-4>"] = {
+      function(cmp)
+        cmp.accept { index = 4 }
+      end,
+    },
+    ["<A-5>"] = {
+      function(cmp)
+        cmp.accept { index = 5 }
+      end,
+    },
+    ["<A-6>"] = {
+      function(cmp)
+        cmp.accept { index = 6 }
+      end,
+    },
+    ["<A-7>"] = {
+      function(cmp)
+        cmp.accept { index = 7 }
+      end,
+    },
+    ["<A-8>"] = {
+      function(cmp)
+        cmp.accept { index = 8 }
+      end,
+    },
+    ["<A-9>"] = {
+      function(cmp)
+        cmp.accept { index = 9 }
+      end,
+    },
+  },
+  appearance = {
+    nerd_font_variant = "mono",
+  },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+  },
+
+  signature = { enabled = true },
+}
+
+return opts
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,17 @@
 local plugins = {
   {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
     "johmsalas/text-case.nvim",
     cmd = {
       "Subs",
@@ -37,85 +49,7 @@ local plugins = {
     event = "InsertEnter",
     dependencies = "rafamadriz/friendly-snippets",
     version = "v0.*",
-    opts = {
-      completion = {
-        accept = {
-          -- experimental auto-brackets support
-          auto_brackets = {
-            enabled = true,
-          },
-        },
-        menu = {
-          scrollbar = false,
-          border = "rounded",
-          draw = {
-            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
-          },
-        },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 200,
-        },
-      },
-      keymap = {
-        preset = "super-tab",
-        ["<CR>"] = { "accept", "fallback" },
-        -- TODO: replace with a loop
-        ["<A-1>"] = {
-          function(cmp)
-            cmp.accept { index = 1 }
-          end,
-        },
-        ["<A-2>"] = {
-          function(cmp)
-            cmp.accept { index = 2 }
-          end,
-        },
-        ["<A-3>"] = {
-          function(cmp)
-            cmp.accept { index = 3 }
-          end,
-        },
-        ["<A-4>"] = {
-          function(cmp)
-            cmp.accept { index = 4 }
-          end,
-        },
-        ["<A-5>"] = {
-          function(cmp)
-            cmp.accept { index = 5 }
-          end,
-        },
-        ["<A-6>"] = {
-          function(cmp)
-            cmp.accept { index = 6 }
-          end,
-        },
-        ["<A-7>"] = {
-          function(cmp)
-            cmp.accept { index = 7 }
-          end,
-        },
-        ["<A-8>"] = {
-          function(cmp)
-            cmp.accept { index = 8 }
-          end,
-        },
-        ["<A-9>"] = {
-          function(cmp)
-            cmp.accept { index = 9 }
-          end,
-        },
-      },
-      appearance = {
-        nerd_font_variant = "mono",
-      },
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-
-      signature = { enabled = true },
-    },
+    opts = require "lua.custom.configs.blink.cmp",
     opts_extend = { "sources.default" },
   },
   {
