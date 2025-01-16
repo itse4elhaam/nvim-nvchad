e4df6c26e58a92f9d3fce86a36af291228c4393c
Elhaam <e4elhaam@gmail.com>
2025-01-16 16:39:38 +0500

    feat (important): changed wakatime code logic to check if the cli exists

--- lua/custom/configs/plenary.lua
+++ lua/custom/configs/plenary.lua
@@ -3,19 +3,31 @@ local config = function()
   local Job = require "plenary.job"
   local async = require "plenary.async"
 
+  -- Path to wakatime-cli
+  local wakatime_cli_path = os.getenv "HOME" .. "/.wakatime/wakatime-cli"
+
+  -- Check if the wakatime-cli exists
+  local function is_wakatime_cli_available()
+    return vim.fn.filereadable(wakatime_cli_path) == 1
+  end
+
   -- Define an asynchronous function to get today's WakaTime usage
-  -- TODO: need to do this without the wakatime cli
   local get_wakatime_time = async.void(function()
+    if not is_wakatime_cli_available() then
+      vim.notify("WakaTime CLI not found at " .. wakatime_cli_path, vim.log.levels.WARN)
+      return
+    end
+
     local tx, rx = async.control.channel.oneshot()
     local ok, job = pcall(Job.new, Job, {
-      command = os.getenv "HOME" .. "/.wakatime/wakatime-cli",
+      command = wakatime_cli_path,
       args = { "--today" },
       on_exit = function(j, _)
         tx(j:result()[1] or "No data")
       end,
     })
     if not ok then
-      vim.notify("Failed to create WakaTime job: " .. job, "error")
+      vim.notify("Failed to create WakaTime job: " .. job, vim.log.levels.ERROR)
       return
     end
 
