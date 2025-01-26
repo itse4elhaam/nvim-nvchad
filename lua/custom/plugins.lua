fe4cdade98ecde9d190c9cb5b89e61400c76205c
Elhaam <e4elhaam@gmail.com>
2025-01-26 10:03:25 +0500

    refactor: moved true-zen into their own conf

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -324,52 +324,7 @@ local plugins = {
       "TZMinimalist",
       "TZAtaraxis",
     },
-    config = function()
-      require("true-zen").setup {
-        modes = {
-          ataraxis = {
-            callbacks = {
-              open_pre = function()
-                vim.cmd "silent !tmux set-option status off"
-              end,
-              open_post = function()
-                vim.cmd "silent !tmux set-option status on"
-              end,
-            },
-          },
-          minimalist = {
-            callbacks = {
-              open_pre = function()
-                vim.cmd "silent !tmux set-option status off"
-              end,
-              open_post = function()
-                vim.cmd "silent !tmux set-option status on"
-              end,
-            },
-          },
-          narrow = {
-            callbacks = {
-              open_pre = function()
-                vim.cmd "silent !tmux set-option status off"
-              end,
-              open_post = function()
-                vim.cmd "silent !tmux set-option status on"
-              end,
-            },
-          },
-          focus = {
-            callbacks = {
-              open_pre = function()
-                vim.cmd "silent !tmux set-option status off"
-              end,
-              open_post = function()
-                vim.cmd "silent !tmux set-option status on"
-              end,
-            },
-          },
-        },
-      }
-    end,
+    config = require "custom.configs.true-zen",
     keys = load_mappings "true_zen",
   },
   {
