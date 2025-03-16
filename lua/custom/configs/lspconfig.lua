2d1e8f8652abace711e9433d2f2d3baf6f09da2b
Elhaam <e4elhaam@gmail.com>
2025-03-16 12:57:54 +0500

    feat: added typescript to tailwind/cssls

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -133,7 +133,19 @@ lspconfig.svelte.setup {
 local servers = { "tailwindcss", "cssls" }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    filetypes = { "css", "html", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact", "svelte" },
+    filetypes = {
+      "css",
+      "html",
+      "javascriptreact",
+      "less",
+      "sass",
+      "scss",
+      "pug",
+      "typescriptreact",
+      "svelte",
+      -- NOTE: can be disabled sometimes
+      "typescript",
+    },
     on_attach = function(client, bufnr)
       disable_formatting(client) -- Disable formatting for specific servers
       on_attach(client, bufnr)
