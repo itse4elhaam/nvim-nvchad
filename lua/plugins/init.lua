df364120e019f77fb37a87db063721c8f5bdffff
Elhaam <e4elhaam@gmail.com>
2025-04-08 21:22:18 +0500

    feat: updated the max_filesize to be considered a large file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -112,7 +112,7 @@ local default_plugins = {
         highlight = {
           enable = true,
           disable = function(lang, buf)
-            local max_filesize = 500 * 1024
+            local max_filesize = 700 * 1024
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize and vim.g.customBigFileOpt then
               return true
