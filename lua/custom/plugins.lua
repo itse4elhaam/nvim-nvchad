c810159e7d28b6e405153e0d64a362acafbd420b
Elhaam <e4elhaam@gmail.com>
2025-06-21 16:16:26 +0500

    chore: added oil.nvim extensions

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -856,16 +856,35 @@ local plugins = {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      default_file_explorer = false,
+    },
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
     config = function()
-      require("oil").setup()
+      require("oil").setup {
+        default_file_explorer = false,
+      }
     end,
   },
+  {
+    "refractalize/oil-git-status.nvim",
+
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+
+    config = true,
+  },
+  {
+    "JezerM/oil-lsp-diagnostics.nvim",
+    dependencies = { "stevearc/oil.nvim" },
+    opts = {},
+  },
+
 
   -- utils
   { "kevinhwang91/promise-async", lazy = false },
