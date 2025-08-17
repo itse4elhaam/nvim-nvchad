cd6a9ffca403177fa6f7231435100e9b82a8fc02
Elhaam <e4elhaam@gmail.com>
2025-08-17 19:15:39 +0500

    feat: opencode.nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -12,6 +12,25 @@ local function get_ai_plugins()
       dependencies = require("custom.configs.codecompanion").dependencies,
       keys = load_mappings "code_companion",
     },
+    {
+      "NickvanDyke/opencode.nvim",
+      dependencies = { "folke/snacks.nvim" },
+      ---@type opencode.Config
+      opts = {
+        -- Your configuration, if any
+      },
+      -- stylua: ignore
+      keys = {
+        { '<leader>ot', function() require('opencode').toggle() end,                           desc = 'Toggle embedded opencode', },
+        { '<leader>oa', function() require('opencode').ask('@cursor: ') end,                   desc = 'Ask opencode',                 mode = 'n', },
+        { '<leader>oa', function() require('opencode').ask('@selection: ') end,                desc = 'Ask opencode about selection', mode = 'v', },
+        { '<leader>op', function() require('opencode').select_prompt() end,                    desc = 'Select prompt',                mode = { 'n', 'v', }, },
+        { '<leader>on', function() require('opencode').command('session_new') end,             desc = 'New session', },
+        { '<leader>oy', function() require('opencode').command('messages_copy') end,           desc = 'Copy last message', },
+        { '<leader>ok', function() require('opencode').command('messages_half_page_up') end,   desc = 'Scroll messages up', },
+        { '<leader>oj', function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
+      },
+    },
   }
 end
 
@@ -353,7 +372,7 @@ local function get_ui_and_visual_plugins()
     },
     {
       "sphamba/smear-cursor.nvim",
-      enabled = true,
+      enabled = false,
       lazy = false,
       opts = require "custom.configs.smear-cursor",
     },
