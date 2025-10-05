84dfdc2ea9dd3179dce4f6215fcfba6c74ed6de8
Elhaam <e4elhaam@gmail.com>
2025-10-05 16:17:29 +0500

    feat: sidekick.nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -22,26 +22,65 @@ local function get_ai_plugins()
       end,
     },
     {
-      "piersolenski/wtf.nvim",
+      "folke/sidekick.nvim",
       lazy = false,
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-telescope/telescope.nvim",
-      },
       opts = {
-        popup_type = "popup",
-        provider = "gemini",
-        providers = {
-          gemini = {
-            api_key = function()
-              return os.getenv "GEMINI_API_KEY"
-            end,
-            model_id = "gemini-2.5-flash",
+        -- add any options here
+        cli = {
+          mux = {
+            backend = "tmux",
+            enabled = true,
           },
         },
       },
-      keys = load_mappings "wtf",
+      -- stylua: ignore
+      keys = {
+        {
+          "<tab>",
+          function()
+            -- if there is a next edit, jump to it, otherwise apply it if any
+            if not require("sidekick").nes_jump_or_apply() then
+              return "<Tab>" -- fallback to normal tab
+            end
+          end,
+          expr = true,
+          desc = "Goto/Apply Next Edit Suggestion",
+        },
+        {
+          "<leader>aa",
+          function() require("sidekick.cli").toggle() end,
+          desc = "Sidekick Toggle CLI",
+        },
+        {
+          "<leader>as",
+          function() require("sidekick.cli").select({ filter = { installed = true } }) end,
+          desc = "Select CLI",
+        },
+        {
+          "<leader>at",
+          function() require("sidekick.cli").send({ msg = "{this}" }) end,
+          mode = { "x", "n" },
+          desc = "Send This",
+        },
+        {
+          "<leader>av",
+          function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+          mode = { "x" },
+          desc = "Send Visual Selection",
+        },
+        {
+          "<leader>ap",
+          function() require("sidekick.cli").prompt() end,
+          mode = { "n", "x" },
+          desc = "Sidekick Select Prompt",
+        },
+        {
+          "<c-.>",
+          function() require("sidekick.cli").focus() end,
+          mode = { "n", "x", "i", "t" },
+          desc = "Sidekick Switch Focus",
+        },
+      },
     },
   }
 end
