2c7271e1d351a0c09bb5c67a7d3a88d8acd40c17
Elhaam <e4elhaam@gmail.com>
2025-10-24 04:01:56 +0500

    Merged feat/statusline-play
    Commits from feat/statusline-play:
    - feat: wakatime at the end with two confs at the start
    
    - wip

--- lua/custom/ui.lua
+++ lua/custom/ui.lua
@@ -5,8 +5,27 @@ local ui = {
     theme = "default",
     separator_style = "round",
     overriden_modules = function(modules)
+      -- Available highlight groups (from statusline/default.lua):
+      -- %#St_NormalMode# - Blue background (mode colors)
+      -- %#St_LspStatus# - White text with background
+      -- %#St_gitIcons# - White text, no background
+      -- %#St_lspError# - Red text
+      -- %#St_lspWarning# - Yellow text
+      -- %#St_lspHints# - Cyan text
+      -- %#St_lspInfo# - Blue text
+      -- %#St_cwd_icon# - Folder icon color
+      -- %#St_cwd_text# - White text with background
+      -- %#St_file_info# - File info colors
+      -- %#St_pos_text# - Position text
+
+      -- Module positions:
+      -- 1=mode, 2=fileInfo, 3=git, 4=%=, 5=LSP_progress, 6=%=, 7=LSP_Diagnostics, 8=LSP_status, 9=cwd, 10=cursor_position
+
       local utils = require "custom.utils"
-      modules[3] = "%#St_gitIcons#  " .. utils.wakatime_stats
+      modules[3] = ""
+      modules[8] = ""
+      modules[9] = ""
+      modules[10] = "%#St_file_txt#" .. utils.wakatime_stats
     end,
   },
   tabufline = {
