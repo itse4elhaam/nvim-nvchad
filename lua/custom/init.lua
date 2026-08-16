b84680273bbd0fe474d63384d4a2bc89df88f44e
Elhaam Basheer Chaudhry <e4elhaam@gmail.com>
2026-08-16 17:30:49 +0500

    chore: remove NES feature flag

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -34,7 +34,6 @@ g.fancy_statusline = os.getenv "NVIM_FANCY_STATUSLINE" == "true"
 g.customBigFileOpt = true
 g.disableFormat = os.getenv "NVIM_DISABLE_FORMAT" == "true"
 g.smear_cursor = os.getenv "NVIM_SMEAR_CURSOR" == "true"
-g.enable_nes = os.getenv "NVIM_ENABLE_NES" == "true"
 g.enable_inlay_hints = false -- Feature flag: disabled by default
 g.nvchad_hot_reload = false
 g.maplocalleader = ","
