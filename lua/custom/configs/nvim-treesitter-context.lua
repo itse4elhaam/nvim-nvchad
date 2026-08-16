632c0c86480fa99f4542b378f3d7e0bedb0270bb
Elhaam Basheer Chaudhry <e4elhaam@gmail.com>
2026-08-16 17:44:32 +0500

    chore: remove obsolete Treesitter Context options

--- lua/custom/configs/nvim-treesitter-context.lua
+++ lua/custom/configs/nvim-treesitter-context.lua
@@ -1,15 +1,7 @@
 return function()
   require("treesitter-context").setup {
     enable = true,
-    throttle = true,
     max_lines = 3,
     trim_scope = "inner",
-    patterns = {
-      default = {
-        "class",
-        "function",
-        "method",
-      },
-    },
   }
 end
