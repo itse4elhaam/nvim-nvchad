11c55966475e02792ab64c96ad90a7c7c9dc97da
Elhaam Basheer Chaudhry <e4elhaam@gmail.com>
2026-08-20 14:52:00 +0500

    fix: name in the title

--- lua/custom/configs/obsidian.lua
+++ lua/custom/configs/obsidian.lua
@@ -15,6 +15,19 @@ return function()
     },
     notes_subdir = "Fleeting",
     new_notes_location = "notes_subdir",
+    note_id_func = function(title)
+      -- Slug the title into the filename with a timestamp prefix to avoid collisions
+      local suffix = ""
+      if title ~= nil then
+        suffix = title:gsub(" ", "-"):gsub("[^%w%-_]", ""):lower()
+      end
+      if suffix == "" then
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
     templates = {
       folder = "templates",
       date_format = "%Y-%m-%d-%a",
